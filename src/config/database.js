1892aa22eee7892d4edeb95f199fb7e78dc84fff
Robson Mendonça <rob.ams@gmail.com>
2020-05-07 19:34:36 -0300

    configurando variaveis de ambiente para deploy

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = 'mongodb://localhost/millionTesouroDireto';
+const uri = process.env.MONGO_URL ? process.env.MONGO_URL : 'mongodb://localhost/millionTesouroDireto';
 
 mongoose.connect(uri,  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 mongoose.Promise = global.Promise;
